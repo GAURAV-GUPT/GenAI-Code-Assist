ddda2ca3e40260a174b44e2fc770157a5212dd05
GAURAV-GUPT <162366227+GAURAV-GUPT@users.noreply.github.com>
2025-08-11 20:57:14 +0100

    Update Integrated.py

--- Integrated.py
+++ Integrated.py
@@ -246,11 +246,11 @@ elif step == "10. Legacy Code Convertor":
     # API_KEY = os.getenv("OPENAI_API_KEY")
 
     # Configure the OpenAI API client
-    if API_KEY:
-        client = OpenAI(api_key=API_KEY)
-    else:
-        st.error("Please set your OPENAI_API_KEY in a .env file.")
-        client = None
+    #if API_KEY:
+     #   client = OpenAI(api_key=API_KEY)
+    #else:
+     #   st.error("Please set your OPENAI_API_KEY in a .env file.")
+      #  client = None
 
 st.set_page_config(page_title="Code Translator", page_icon="📝")
 st.title("Legacy Code Translator ⚙️")
@@ -340,4 +340,5 @@ if st.button("🚀 Translate Code"):
             st.code(translated_code, language="csharp")
     else:
         st.warning("Please enter some source code to translate.")
+
 
